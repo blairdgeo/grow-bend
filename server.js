c3dbc427e16f83095b91b99606d1040c837312c5
mikemillspdx <mikemillspdx@gmail.com>
2015-11-17 14:32:14 -0800

    localhost 1337

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var restify = require('restify');
 var siteLoader = require('./scripts/server/lib/controllers/siteLoader.js');
 
 var server = restify.createServer();
-var port = process.env.PORT || 82;
+var port = process.env.PORT || 1337;
 server.use(restify.queryParser());
 server.get('/sendEmail', siteLoader.sendEmail);
 server.get('/.*', siteLoader.loadsite);
